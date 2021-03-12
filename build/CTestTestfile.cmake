c48a124ed7ef2e96a7cef97ccda22695e75b4f6d
sajith <sajith@digitalxlabs.com>
2021-03-12 06:58:03 +0530

    Init

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/sajith/Robotics/ros_example/src
+# Build directory: /home/sajith/Robotics/ros_example/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
